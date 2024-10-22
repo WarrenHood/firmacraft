a0d54cd44f20ab5ca964120919987ccb3f4162fd
Warren Hood <nullbyte001@gmail.com>
2024-10-22 20:21:03 +0200

    Remove all create recipes

--- kubejs/server_scripts/src/createStuff.js
+++ kubejs/server_scripts/src/createStuff.js
@@ -1,13 +1,19 @@
 ServerEvents.recipes(event => {
-    // Replace all usages of andesite with raw andesite in the Create mod
-    event.replaceInput(
-        {
-            mod: "create",
-            input: "minecraft:andesite"
-        },
-        "minecraft:andesite",
-        "tfc:rock/raw/andesite"
-    )
+    // // Replace all usages of andesite with raw andesite in the Create mod
+    // Remove all create recipes for now
+    // Maybe at some point I'll add some recipes to make it compatible
+    event.remove({
+        mod: "create"
+    })
+    
+    // event.replaceInput(
+    //     {
+    //         mod: "create",
+    //         input: "minecraft:andesite"
+    //     },
+    //     "minecraft:andesite",
+    //     "tfc:rock/raw/andesite"
+    // )
 
     console.log("Create recipes have been replaced!")
 })
