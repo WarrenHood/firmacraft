8ed37076f96738be6f13ad5367fe8285a4ea62cb
Warren Hood <nullbyte001@gmail.com>
2024-10-22 19:53:02 +0200

    Replace all usages of andesite in the create mod with raw andesite from tfc

--- README.md
+++ README.md
@@ -8,6 +8,11 @@ This modpack is managed by [mcmpmgr](https://github.com/WarrenHood/MCModpackMana
 
 For easy installation via a GUI, create a profile in `mmm` (GUI frontend for `mcmpmgr`) pointing to this repo.
 
+## Recipes
+
+The recipe integration is still very much WIP.
+
+
 # Voice Chat Setup
 
 This requires that UDP port 24454 is open.
--- kubejs/server_scripts/src/createStuff.js
+++ kubejs/server_scripts/src/createStuff.js
@@ -1,7 +1,10 @@
 ServerEvents.recipes(event => {
-    // Replace all usages of andesite with raw andesite
+    // Replace all usages of andesite with raw andesite in the Create mod
     event.replaceInput(
-        { input: "minecraft:andesite" },
+        {
+            mod: "create",
+            input: "minecraft:andesite"
+        },
         "minecraft:andesite",
         "tfc:rock/raw/andesite"
     )
